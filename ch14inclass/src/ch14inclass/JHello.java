0860ec878f2ae58187b33ab9b3759d3ed348d08a
XuYangFang <xuyangfff@gmail.com>
2022-11-03 19:26:06 -0500

    JHello

--- ch14inclass/src/ch14inclass/JHello.java
+++ ch14inclass/src/ch14inclass/JHello.java
@@ -3,7 +3,8 @@ package ch14inclass;
 public class JHello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JHelloFrame frame = new JHelloFrame();
+		frame.setVisible(true);
 
 	}
 
